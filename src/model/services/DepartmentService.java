e9a152ae46a697a5b12d75b7606b5bd08d4ac295
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-27 14:56:24 -0300

    Ação de inicialização como parâmetro (Consumer)

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -8,9 +8,9 @@ import model.entities.Department;
 public class DepartmentService {
 	public List<Department> findAll() {
 		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Electronics"));
+		list.add(new Department(1, "Livros"));
+		list.add(new Department(2, "Computadores"));
+		list.add(new Department(3, "Eletrônicos"));
 		return list;
 	}
 }
