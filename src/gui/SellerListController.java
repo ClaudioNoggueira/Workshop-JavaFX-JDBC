067405714d8902eb98ab92cdbc55431e8a67c149
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-28 14:59:55 -0300

    SellertList TableView

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -41,6 +42,15 @@ public class SellerListController implements Initializable, DataChangeListener {
 	private TableColumn<Seller, String> tableColumnName;
 
 	@FXML
+	private TableColumn<Seller, String> tableColumnEmail;
+
+	@FXML
+	private TableColumn<Seller, Date> tableColumnBirthDate;
+
+	@FXML
+	private TableColumn<Seller, Double> tableColumnSalary;
+
+	@FXML
 	private Button btnInsert;
 
 	@FXML
@@ -70,6 +80,11 @@ public class SellerListController implements Initializable, DataChangeListener {
 	private void initializeNodes() {
 		tableColumnID.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+		tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+		Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+		tableColumnSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+		Utils.formatTableColumnDouble(tableColumnSalary, 2);
 
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
@@ -129,8 +144,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 					return;
 				}
 				setGraphic(button);
-				button.setOnAction(
-						event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+				button.setOnAction(event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
 			}
 		});
 	}
--- src/model/services/SellerService.java
+++ src/model/services/SellerService.java
@@ -1,7 +1,11 @@
 package model.services;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerService {
@@ -10,8 +14,18 @@ public class SellerService {
 
 	public List<Seller> findAll() {
 		// return dao.findAll();
-		return null;
-	}	
+		Date date = Calendar.getInstance().getTime();
+		List<Seller> list = new ArrayList<>();
+
+		list.add(new Seller(1, "Maria", "maria@gmail.com", date, 2500.0, new Department()));
+		list.add(new Seller(2, "Alex", "alex@gmail.com", date, 2000.0, new Department()));
+		list.add(new Seller(3, "Bob", "bob@gmail.com", date, 2000.0, new Department()));
+		list.add(new Seller(4, "Jebediah", "jebediah@gmail.com", date, 3000.0, new Department()));
+		list.add(new Seller(5, "Jesus", "jesus@gmail.com", date, 4500.0, new Department()));
+		
+		
+		return list;
+	}
 
 	public void SaveOrUpdate(Seller obj) {
 		if (obj.getId() == null) {
